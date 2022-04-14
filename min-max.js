e946fb8be3103939dba60f5a45c9a96c326d86b8
kamranatta <kamran.creativetech@gmail.com>
2022-04-14 23:15:33 +0500

    update

--- min-max.js
+++ min-max.js
@@ -1,21 +1,34 @@
-var checkout = document.querySelector('.form-control__button');
-console.log('Value:::', checkout);
+<script src="https://djqizrxa6f10j.cloudfront.net/ecwid-sdk/js/1.2.1/ecwid-app.js"></script>
+// Initialize the application
+EcwidApp.init({
+    app_id: "custom-app-71585715-1", 
+    autoloadedflag: true, 
+    autoheight: true
+    });
 
-checkout.addEventListener('click', function(event) {
-    console.log('Button is clickedddddd!!!!!!!!!!!!!!!!!!!!!!');
+Ecwid.OnPageLoaded.add(function(page){
+    console.log("Current page is of type: " + page.type);
+    if(page.type == "PRODUCT"){
+        var checkout = document.querySelector('.form-control__button');
+        console.log('Value:::', checkout);
 
-    // const span = document.getElementsByClassName('details-product-attribute__value');
-    const parent = document.querySelector('.details-product-attribute');
-    const quantitySet = parseInt(parent.querySelector('.details-product-attribute__value').textContent);
+        checkout.addEventListener('click', function(event) {
+            console.log('Button is clickedddddd!!!!!!!!!!!!!!!!!!!!!!');
 
-    var quantity = parseInt(document.getElementById("qty-field").value);
-    console.log('Clicked!!');
-    console.log('quantityset!!', quantitySet);
-    console.log('Quantity!!', quantity);
+            // const span = document.getElementsByClassName('details-product-attribute__value');
+            const parent = document.querySelector('.details-product-attribute');
+            const quantitySet = parseInt(parent.querySelector('.details-product-attribute__value').textContent);
 
-    if (!quantity || quantity < quantitySet) {
-        event.preventDefault();
-        alert('Please add equal to or more items than minimum quantity');
-        // document.querySelector('.a-card').style.display = 'block';
+            var quantity = parseInt(document.getElementById("qty-field").value);
+            console.log('Clicked!!');
+            console.log('quantityset!!', quantitySet);
+            console.log('Quantity!!', quantity);
+
+            if (!quantity || quantity < quantitySet) {
+                event.preventDefault();
+                alert('Please add equal to or more items than minimum quantity');
+                // document.querySelector('.a-card').style.display = 'block';
+            }
+        });
     }
 });
